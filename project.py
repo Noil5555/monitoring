1262920534800d240051ad173cf54b7f145d3818
Noil5555 <lionlucawitt@gmail.com>
2026-02-24 12:54:37 +0100

    Update project.py

--- project.py
+++ project.py
@@ -61,28 +61,6 @@ while True:
     from evaluator import evaluate
     from notifier import send_mail
    
-    ##################################################################################
-    # Warnings
-    ##################################################################################
-
-    # CPU Warning
-    if cpu_usage["total"] >= 90:
-        cpu_status = "Critical"
-    elif cpu_usage["total"] >= 80:
-        cpu_status = "Warning"
-    elif cpu_usage["total"] <= 80:
-        cpu_status = "Okay"
-    else:
-        with open(text.file, 'rb') as fp:
-            msg = MIMEText(fp.read())
-        msg['Betreff'] = 'Unable to read CPU status' 
-        msg['Von'] = sender
-        msg['An'] = adresser
-
-        s = smtplib.SMTP('localhost')
-        s.sendmail(sender, [adresser], msg.as_string())
-        s.quit()
-        break
     
     cpu_notification()
        
