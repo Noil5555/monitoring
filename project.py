71cdd4d2530a181671fa2f953a037cb0a09cc662
Noil5555 <lionlucawitt@gmail.com>
2026-02-24 12:55:19 +0100

    Update project.py

--- project.py
+++ project.py
@@ -32,29 +32,6 @@ disk_logging = ""
 disk_status = ""
 
 
-def cpu_notification():
-    if cpu_logging == "":
-        cpu_logging = cpu_status
-    elif cpu_logging != cpu_status:
-        with open(text.file, 'rb') as fp:
-            msg = MIMEText(fp.read())
-        msg['Betreff'] = 'Monitoring Warning CPU '+ cpu_logging
-        msg['Von'] = sender
-        msg['An'] = adresser
-
-        s = smtplib.SMTP('localhost')
-        s.sendmail(sender, [adresser], msg.as_string())
-        s.quit()
-
-        cpu_logging = cpu_status
-    else:
-        with open(text.file, 'rb') as fp:
-            msg = MIMEText(fp.read())
-        msg['Betreff'] = 'Somthing want wrong'
-        msg['Von'] = sender
-        msg['An'] = adresser
-
-
 while True:
 
     from hardware-info import get_cpu_usage
