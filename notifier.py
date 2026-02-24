63368039fa0f3006c043c412a00840c2fe6cc099
Noil5555 <lionlucawitt@gmail.com>
2026-02-24 12:55:33 +0100

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,1 +1,21 @@
+def cpu_notification():
+    if cpu_logging == "":
+        cpu_logging = cpu_status
+    elif cpu_logging != cpu_status:
+        with open(text.file, 'rb') as fp:
+            msg = MIMEText(fp.read())
+        msg['Betreff'] = 'Monitoring Warning CPU '+ cpu_logging
+        msg['Von'] = sender
+        msg['An'] = adresser
 
+        s = smtplib.SMTP('localhost')
+        s.sendmail(sender, [adresser], msg.as_string())
+        s.quit()
+
+        cpu_logging = cpu_status
+    else:
+        with open(text.file, 'rb') as fp:
+            msg = MIMEText(fp.read())
+        msg['Betreff'] = 'Somthing want wrong'
+        msg['Von'] = sender
+        msg['An'] = adresser
